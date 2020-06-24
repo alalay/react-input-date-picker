274b0c159f9c18afb98cb507a7a1c8548039889d
Yan Wang <ywang@talend.com>
2020-06-24 11:28:28 +0800

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-input-date-picker'
+import 'react-input-date-picker/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
