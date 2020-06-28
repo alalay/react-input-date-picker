629da3602b39749be678f8c32ef8f2e1020a308f
alalay <alalay888@gmail.com>
2020-06-28 16:02:06 +0800

    HeaderTitle: container

---
+++ src/picker/HeaderTitle/HeaderTitle.component.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import theme from './HeaderTitle.css';
+
+function HeaderTitle(props) {
+  return <div className={theme.header}>Mars 2020</div>;
+}
+
+export default HeaderTitle;
