0f9d046e0d642333e70ec265991ce427d782bfd5
alalay <alalay888@gmail.com>
2020-06-29 17:50:08 +0800

    feat: use global scope style temporaly

--- src/InputDatePicker/Picker/Picker.component.js
+++ src/InputDatePicker/Picker/Picker.component.js
@@ -3,6 +3,8 @@ import DateView from '../../views/DateView';
 
 import theme from './Picker.scss';
 
+import '../../shared/styles/common.scss';
+
 function Picker(props) {
   return (
     <div className={theme.container} aria-label='Date picker'>
--- src/views/DateView/HeaderTitle/HeaderTitle.component.js
+++ src/views/DateView/HeaderTitle/HeaderTitle.component.js
@@ -6,16 +6,13 @@ import setMonth from 'date-fns/setMonth';
 import format from 'date-fns/format';
 
 import theme from './HeaderTitle.css';
-import common from '../../../shared/styles/common.scss';
 
 function HeaderTitle(props) {
   const date = setYear(setMonth(new Date(0), props.monthIndex), props.year);
   const label = format(date, 'MMMM yyyy');
   return (
     <div className={theme.header}>
-      <button className={`${common.btn} ${common['btn-tertiary']}`}>
-        {label}
-      </button>
+      <button className='btn btn-tertiary'>{label}</button>
     </div>
   );
 }
