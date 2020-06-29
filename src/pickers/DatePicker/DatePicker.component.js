09dd21352aade3f5a8f479db70272e4a6ebce270
alalay <alalay888@gmail.com>
2020-06-29 22:38:05 +0800

    feat: DatePicker skeleton

--- src/pickers/DatePicker/DatePicker.component.js
+++ src/pickers/DatePicker/DatePicker.component.js
@@ -1,7 +1,32 @@
 import React from 'react';
 
 function DatePicker(props) {
-  return <p>DatePicker</p>;
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>MO</th>
+          <th>MO</th>
+          <th>MO</th>
+          <th>MO</th>
+          <th>MO</th>
+          <th>MO</th>
+          <th>MO</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
 
 export default DatePicker;
