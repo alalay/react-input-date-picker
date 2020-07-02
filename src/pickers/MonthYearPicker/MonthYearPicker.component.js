f689cb7cbdebe82ab180510dfd4bb660271543ba
alalay <alalay888@gmail.com>
2020-07-02 11:56:35 +0800

    feat: MonthPicker - template

--- src/pickers/MonthYearPicker/MonthYearPicker.component.js
+++ src/pickers/MonthYearPicker/MonthYearPicker.component.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 function MonthYearPicker(props) {
-  return <p>MonthYearPicker</p>;
+  return (
+    <table>
+      <tbody>
+        <tr><td>Jan</td><td>Jan</td><td>Jan</td></tr>
+        <tr><td>Jan</td><td>Jan</td><td>Jan</td></tr>
+        <tr><td>Jan</td><td>Jan</td><td>Jan</td></tr>
+        <tr><td>Jan</td><td>Jan</td><td>Jan</td></tr>
+      </tbody>
+    </table>
+  );
 }
 
 export default MonthYearPicker;
