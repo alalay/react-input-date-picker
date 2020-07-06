b4b3896714082a0c478d2b79e3ad189323d232ea
alalay <alalay888@gmail.com>
2020-07-06 17:17:56 +0800

    feat: ViewLayout

--- src/views/DateView/DateView.component.js
+++ src/views/DateView/DateView.component.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Header from './Header';
 import HeaderTitle from './HeaderTitle';
 import DatePicker from '../../pickers/DatePicker';
+import ViewLayout from '../ViewLayout';
 
 /**
  * Get the positive euclidean modulo number from a dividend and a divisor
@@ -34,32 +34,42 @@ function DateView(props) {
   function goNext() {
     incrementMonthIndex(1);
   }
+
+  const header = {
+    leftElement: (
+      <button
+        className='btn btn-tertiary btn-icon-only'
+        onClick={() => goPrevious()}
+      >
+        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'>
+          <path d='M15 7H4.4l4-4L7 1.6.6 8 7 14.4 8.4 13l-4-4H15z' />
+        </svg>
+      </button>
+    ),
+    middleElement: (
+      <HeaderTitle {...props.calendar} onTitleClick={props.onTitleClick} />
+    ),
+    rightElement: (
+      <button
+        className='btn btn-tertiary btn-icon-only'
+        onClick={() => goNext()}
+      >
+        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'>
+          <path d='M1 9h10.7l-4 4 1.4 1.4L15.5 8 9.1 1.6 7.7 3l4 4H1z' />
+        </svg>
+      </button>
+    )
+  };
+  const bodyElement = (
+    <DatePicker
+      calendar={props.calendar}
+      selectedDate={props.selectedDate}
+      onSelect={props.onSelectDate}
+    />
+  );
   return (
     <div className='dateview'>
-      <Header
-        left={
-          <button className='btn btn-tertiary' onClick={() => goPrevious()}>
-            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'>
-              <path d='M15 7H4.4l4-4L7 1.6.6 8 7 14.4 8.4 13l-4-4H15z' />
-            </svg>
-          </button>
-        }
-        middle={
-          <HeaderTitle {...props.calendar} onTitleClick={props.onTitleClick} />
-        }
-        right={
-          <button className='btn btn-tertiary' onClick={() => goNext()}>
-            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'>
-              <path d='M1 9h10.7l-4 4 1.4 1.4L15.5 8 9.1 1.6 7.7 3l4 4H1z' />
-            </svg>
-          </button>
-        }
-      />
-      <DatePicker
-        calendar={props.calendar}
-        selectedDate={props.selectedDate}
-        onSelect={props.onSelectDate}
-      />
+      <ViewLayout header={header} bodyElement={bodyElement} />
     </div>
   );
 }
--- src/views/MonthYearView/MonthYearView.component.js
+++ src/views/MonthYearView/MonthYearView.component.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Header from '../DateView/Header';
 import HeaderTitle from '../DateView/HeaderTitle';
 import MonthYearPicker from '../../pickers/MonthYearPicker';
+import ViewLayout from '../ViewLayout';
 
 function MonthYearView(props) {
   function goToMonthDate() {
@@ -12,24 +12,23 @@ function MonthYearView(props) {
   function onSelectMonth(monthIndex) {
     props.onSelectMonth(monthIndex);
   }
-  return (
-    <div className='monthYearView'>
-      <Header
-        left={
-          <button className='btn btn-tertiary' onClick={goToMonthDate}>
-            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'>
-              <path d='M15 7H4.4l4-4L7 1.6.6 8 7 14.4 8.4 13l-4-4H15z' />
-            </svg>
-          </button>
-        }
-        middle={<HeaderTitle {...props.calendar} />}
-      />
-      <MonthYearPicker
-        calendar={props.calendar}
-        onSelectMonth={onSelectMonth}
-      />
-    </div>
+  const header = {
+    leftElement: (
+      <button
+        className='btn btn-tertiary btn-icon-only'
+        onClick={goToMonthDate}
+      >
+        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'>
+          <path d='M15 7H4.4l4-4L7 1.6.6 8 7 14.4 8.4 13l-4-4H15z' />
+        </svg>
+      </button>
+    ),
+    middleElement: <HeaderTitle {...props.calendar} />
+  };
+  const bodyElement = (
+    <MonthYearPicker calendar={props.calendar} onSelectMonth={onSelectMonth} />
   );
+  return <ViewLayout header={header} bodyElement={bodyElement} />;
 }
 
 MonthYearView.propTypes = {
