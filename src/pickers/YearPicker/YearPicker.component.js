7364c0efdb943d654cd37e2fd1aa46bef2a57b20
alalay <alalay888@gmail.com>
2020-07-10 21:55:32 +0800

    YearPicker - scroll up/down

--- src/pickers/YearPicker/YearPicker.component.js
+++ src/pickers/YearPicker/YearPicker.component.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { buildYears } from '../../generator';
 
 import theme from './YearPicker.scss';
 
+const YEAR_WINDOW_OVERFLOW_SIZE = 3;
+
 function YearPicker(props) {
-  const years = buildYears();
+  const [years, setYears] = useState(buildYears());
+  function getMiddleYear() {
+    return years[Math.floor(years.length / 2)];
+  }
+  function scroll(pace) {
+    setYears(buildYears(getMiddleYear() + pace, YEAR_WINDOW_OVERFLOW_SIZE));
+  }
+  function onScrollUp() {
+    scroll(-1);
+  }
+  function onScrollDown() {
+    scroll(1);
+  }
   return (
     <div className={theme['year-picker']}>
+      <button onClick={onScrollUp}>up</button>
       <ol>
         {years.map((year, index) => (
           <li key={index}>
@@ -20,6 +35,7 @@ function YearPicker(props) {
           </li>
         ))}
       </ol>
+      <button onClick={onScrollDown}>down</button>
     </div>
   );
 }
