4b416187698ab2e64e54158fff2e91ed3fc980c9
alalay <alalay888@gmail.com>
2020-07-24 10:58:10 +0800

    dropdown

--- src/views/DateView/Dropdown/Dropdown.component.js
+++ src/views/DateView/Dropdown/Dropdown.component.js
@@ -1,11 +1,41 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
+import React, { useState, useContext } from 'react';
 import classNames from 'classnames';
-import { useUncontrolledProp } from 'uncontrollable';
 
 import DropdownContext from './DropdownContext';
 
+function useDropdownMenu() {
+  const { show, toggle } = useContext(DropdownContext);
+  return {
+    show,
+    close: () => toggle(false)
+  };
+}
+function Menu({ children }) {
+  const { show, close, props } = useDropdownMenu();
+  const dropdownClass = classNames('dropdown-menu', { open: show });
+  return (
+    <div className={dropdownClass} onClick={close}>
+      {children}
+    </div>
+  );
+}
+
+function useDropdownToggle() {
+  const { toggle, show } = useContext(DropdownContext);
+  return [{}, { toggle, show }];
+}
+function Toggle({ children }) {
+  const [props, { toggle }] = useDropdownToggle();
+  return (
+    <button {...props} onClick={toggle}>
+      {children}
+    </button>
+  );
+}
+
 function Dropdown({ children }) {
   const [show, onToggle] = useState(false);
+
   const context = {
     show,
     toggle: () => onToggle(!show)
@@ -19,40 +49,28 @@ function Dropdown({ children }) {
 
 function DropdownButton(props) {
   const { title, children } = props;
-  const [isOpen, setOpen] = useState(false);
-  const ref = useRef(null);
-  const dropdownClass = classNames('dropdown', { open: isOpen });
 
-  function handleDocumentClick(event) {
-    if (!ref.current.contains(event.target)) {
-      setOpen(false);
-    }
-  }
-  useEffect(() => {
-    document.addEventListener('click', handleDocumentClick, false);
-    return () =>
-      document.removeEventListener('click', handleDocumentClick, false);
-  });
-  function onToggle() {
-    setOpen(!isOpen);
-  }
+  // function handleDocumentClick(event) {
+  //   if (!ref.current.contains(event.target)) {
+  //     setOpen(false);
+  //   }
+  // }
+  // useEffect(() => {
+  //   document.addEventListener('click', handleDocumentClick, false);
+  //   return () =>
+  //     document.removeEventListener('click', handleDocumentClick, false);
+  // });
 
-  const onSelect = useCallback((...args) => {
-    setOpen(false);
-  });
   return (
-    <div className={dropdownClass} ref={ref}>
-      <div>
-        <button className='btn btn-info btn-tertiary' onClick={onToggle}>
-          {title}
-        </button>
-      </div>
-      <div className='dropdown-menu'>
-        <div onClick={onSelect}>{children}</div>
-      </div>
-    </div>
+    <Dropdown>
+      {({ props }) => (
+        <div {...props} className='dropdown'>
+          <Toggle>{title}</Toggle>
+          <Menu>{children}</Menu>
+        </div>
+      )}
+    </Dropdown>
   );
 }
-
 
 export default DropdownButton;
