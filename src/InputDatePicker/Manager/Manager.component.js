494cd5bae248f58bd92d9dc52eda8842a2132621
alalay <alalay888@gmail.com>
2020-07-27 15:03:44 +0800

    highlight selected date

--- src/InputDatePicker/Manager/Manager.component.js
+++ src/InputDatePicker/Manager/Manager.component.js
@@ -5,7 +5,14 @@ import { DateContext } from '../Context';
 import { extractFromDate, extractDate } from '../date-extraction';
 
 function Manager(props) {
-  const [state, setState] = useState(() => extractDate(props.value));
+  function getDateOptions() {
+    return {
+      dateFormat: props.dateFormat
+    };
+  }
+  const [state, setState] = useState(() =>
+    extractDate(props.value, getDateOptions())
+  );
   function onPickerChange(event, { date }) {
     const nextState = extractFromDate(date);
     setState(nextState);
@@ -14,7 +21,7 @@ function Manager(props) {
   return (
     <DateContext.Provider
       value={{
-        value: { textInput: state.textInput, date: state.localDate },
+        value: { textInput: state.textInput, date: state.date },
         inputManagement: { onChange: () => {} },
         pickerManagement: { onSubmit: onPickerChange }
       }}
@@ -26,6 +33,9 @@ function Manager(props) {
 
 Manager.propTypes = {
   value: PropTypes.string
+};
+Manager.defaultProps = {
+  dateFormat: 'yyyy-MM-dd'
 };
 
 export default Manager;
