4c912a3135fce81b561d1df0b6011f355c86bb79
alalay <alalay888@gmail.com>
2020-07-28 14:50:49 +0800

    fix lint

--- src/pickers/CalendarPicker/CalenderPicker.component.js
+++ src/pickers/CalendarPicker/CalenderPicker.component.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import getMonth from 'date-fns/getMonth';
 import getYear from 'date-fns/getYear';
